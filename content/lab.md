6bae226ac6c3ada733f7240ca5111c67729c298a
Romain Clement <contact@romain-clement.net>
2023-03-28 19:15:24 +0200

    Embed JupyterLite within website

---
+++ content/lab.md
@@ -0,0 +1,20 @@
+# 🧪 Lab
+
+## JupyterLite
+
+Want to use the Python programming language right now?
+Use the following tools right in your browser, thanks to the
+[JupyterLite](https://jupyterlite.readthedocs.io) project!
+
+[Go to JupyterLite](/jupyterlite/){ .md-button }
+
+## REPL
+
+You can use the following REPL Python terminal:
+
+<iframe
+  src="/jupyterlite/repl/?toolbar=1&kernel=python&code="
+  width="100%"
+  height="400px"
+>
+</iframe>
--- hooks.py
+++ hooks.py
@@ -1,6 +1,14 @@
+import logging
+import subprocess
+import time
 import jinja2
 
+from pathlib import Path
 from typing import Any
+from mkdocs.config.defaults import MkDocsConfig
+
+
+logger = logging.getLogger("mkdocs.hooks")
 
 
 def do_startswith(value: str, prefix: str) -> bool:
@@ -9,3 +17,23 @@ def do_startswith(value: str, prefix: str) -> bool:
 
 def on_env(env: jinja2.Environment, **kwargs: Any) -> None:
     env.tests["startswith"] = do_startswith
+
+
+def jupyterlite_build(output_dir: str) -> None:
+    logger.info(f"Building JupyterLite...")
+    output_dir = Path(output_dir) / "jupyterlite"
+    lite_dir = Path(__file__).parent / "jupyterlite"
+
+    start = time.monotonic()
+    result = subprocess.run(
+        ("jupyter", "lite", "build", "--lite-dir", lite_dir, "--output-dir", output_dir),
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    result.check_returncode()
+
+    logger.info(f"JupyterLite built in {time.monotonic() - start:.2f} seconds")
+
+
+def on_post_build(config: MkDocsConfig) -> None:
+    jupyterlite_build(config.site_dir)
